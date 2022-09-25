4a0024a7cbe08d5ddc6fbea4ededef6e0b9bc97b
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-09-25 11:03:37 -0300

    feat: add outline truck icon

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,9 +1,11 @@
 import { FiMenu , } from '@meronex/icons/fi'
 import { AiOutlineUser } from '@meronex/icons/ai'
 import { BiShoppingBag } from '@meronex/icons/bi';
+import { FiTruck } from '@meronex/icons/fi'
 
 export const icons = {
   menu: FiMenu ,
   outlineUser: AiOutlineUser,
-  shoppingBag: BiShoppingBag
+  shoppingBag: BiShoppingBag,
+  outlineTruck: FiTruck
 }
