95168f20921bff2cf6a38db96924a4918ec28bda
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-01 20:14:30 -0300

    refactor(my account button): remove user name from button label

--- components/Header/components/my-account-button/index.tsx
+++ components/Header/components/my-account-button/index.tsx
@@ -1,22 +1,12 @@
 import { Button } from "components";
 
-import { useAuth } from "context";
-
-import * as Styles from "./styles";
-
 export function MyAccountButton() {
-  const auth = useAuth();
-
-  const label = auth?.user ? `Olá, \n ${auth.user?.username}` : "Minha conta";
-
   return (
     <Button
       variant="letter"
       icon={{ name: "outlineUser" }}
     >
-      <Styles.Label>
-        {label}
-      </Styles.Label>
+      Minha conta
     </Button>
   );
 }
--- components/Header/components/my-account-button/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from "stitches.config";
-
-export const Label = styled("span", {
-  textOverflow: "ellipsis",
-  display: "inline-block",
-  overflow: "hidden",
-  maxWidth: "5rem",
-  textAlign: "start",
-});
