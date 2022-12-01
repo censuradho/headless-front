fccba8144ec120019f48a60a45a79812bd7bb913
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-01 07:10:06 -0300

    feat(purchase resume): add initial structure

--- layout/checkout/components/order-resume/index.tsx
+++ layout/checkout/components/order-resume/index.tsx
@@ -1,17 +1,71 @@
-import { Box, Icon, Typography } from "components";
+import {
+  Box, Button, Icon, Typography,
+} from "components";
+import { useCart } from "context";
+import { memo, useMemo } from "react";
+import { uuid } from "utils";
+import { ProductPreview } from "./components";
 import * as Styles from "./styles";
 
-export function OrderResume() {
+export const OrderResume = memo(() => {
+  const { cart } = useCart();
+
+  const renderProductPreview = () => {
+    const products = Object
+      .entries(cart)
+      .map(([, value]) => value);
+
+    return products.map((product) => Object
+      .entries(product.inventories)
+      .map(([, inventory]) => (
+        <li
+          key={uuid()}
+        >
+          <ProductPreview
+            inventory={inventory}
+            product={product}
+          />
+        </li>
+      )));
+  };
+
+  const subTotal = useMemo(() => {
+    const products = Object
+      .entries(cart)
+      .map(([key, value]) => value);
+
+    return products.map((product) => {
+      const entriesPrice = Object
+        .entries(product?.inventories || {})
+        .map(([, value]) => value.quantity * product.price);
+
+      return entriesPrice?.reduce((prev, next) => prev + next, 0);
+    })
+      .reduce((prev, next) => prev + next, 0);
+  }, [cart]);
+
+  const total = subTotal;
+
   return (
     <Styles.Container>
       <Box flexDirection="column" gap={0.8}>
-        <Box justifyContent="space-between">
-          <Box gap={1} alignItems="center">
-            <Icon name="shoppingBag" />
-            <Typography variant="sub-headline">Resumo da compra</Typography>
-          </Box>
+        <Box gap={1} alignItems="center">
+          <Icon name="shoppingBag" />
+          <Typography variant="sub-headline">Resumo da compra</Typography>
         </Box>
+        <Styles.ProductPreviewList>{renderProductPreview()}</Styles.ProductPreviewList>
+      </Box>
+      <Styles.SummaryItem>
+        <Typography>Subtotal:</Typography>
+        <Typography>{subTotal}</Typography>
+      </Styles.SummaryItem>
+      <Styles.SummaryItem>
+        <Typography semiBold>Total:</Typography>
+        <Typography semiBold>{total}</Typography>
+      </Styles.SummaryItem>
+      <Box marginTop={2}>
+        <Button fullWidth>Finalizar compra</Button>
       </Box>
     </Styles.Container>
   );
-}
+});
--- layout/checkout/components/order-resume/styles.ts
+++ layout/checkout/components/order-resume/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled("div", {
   border: "1px solid $highlight",
   padding: "1rem",
   width: "100%",
+  height: "max-content",
   maxWidth: "25rem",
 });
 
@@ -17,4 +18,18 @@ export const AnchorUnderline = styled("a", {
   background: "none",
   textDecoration: "underline",
   fontSize: "$caption2",
+});
+
+export const ProductPreviewList = styled("ul", {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+});
+
+export const SummaryItem = styled("div", {
+  display: "flex",
+  justifyContent: "space-between",
+  borderBottom: "1px solid $highlight",
+  alignItems: "center",
+  padding: "0.5rem 0",
 });
