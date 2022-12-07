ca4bfeaf7ad0c9186e5d8d0befff06a2cb3c9e5e
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-07 12:33:13 -0300

    feat(usePaymentMethod): added test card number

--- hooks/entries/usePaymentMethod/validations.ts
+++ hooks/entries/usePaymentMethod/validations.ts
@@ -1,9 +1,10 @@
 import { MESSAGES } from "constants/validations";
+import { getCardFlag } from "utils";
 import * as y from "yup";
 
 export const PaymentMethodSchemaValidation = y.object({
   installments: y.string().required(MESSAGES.REQUIRED),
-  creditCardNumber: y.string().required(MESSAGES.REQUIRED),
+  creditCardNumber: y.string().required(MESSAGES.REQUIRED).test("len", MESSAGES.INCORRECT_FORMAT, (val) => !!getCardFlag(val || "")),
   holderName: y.string().required(MESSAGES.REQUIRED),
   expMonth: y.string().required(MESSAGES.REQUIRED),
   expYear: y.string().required(MESSAGES.REQUIRED),
