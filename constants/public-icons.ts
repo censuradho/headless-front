009cae7b90e3af2e0d98ae76b0b51d2a78df9d05
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-07 09:28:29 -0300

    feat(public icons):

---
+++ constants/public-icons.ts
@@ -0,0 +1,15 @@
+import Mastercard from "public/icons/card-flags/mastercard.svg";
+import Amex from "public/icons/card-flags/amex.svg";
+import Diners from "public/icons/card-flags/diners.svg";
+import Elo from "public/icons/card-flags/elo.svg";
+import Pix from "public/icons/card-flags/pix.svg";
+import Visa from "public/icons/card-flags/visa.svg";
+
+export const publicIcons = {
+  cardMastercard: Mastercard,
+  cardAmex: Amex,
+  cardDiners: Diners,
+  cardElo: Elo,
+  cardPix: Pix,
+  cardVisa: Visa,
+};
