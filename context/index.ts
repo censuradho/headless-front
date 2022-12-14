16c8fa19283f1351087bbcec343e3513d3e1c2ae
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-14 15:51:44 -0300

    fix(_app page): fix contexts import

--- context/index.ts
+++ context/index.ts
@@ -2,3 +2,4 @@ export * from "./theme";
 export * from "./auth";
 export * from "./cart";
 export * from "./checkout";
+export * from "./toast";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import {
   AuthProvider,
   ThemeProvider,
   ToastProvider,
