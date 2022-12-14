d78aa180ef136c1327b2f8ea8b1baa6348efa26b
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-14 15:20:47 -0300

    fix(errors)

--- components/Header/components/index.ts
+++ components/Header/components/index.ts
@@ -1,3 +1,3 @@
-export { Navigation } from "./navigation";
+export { Navigation } from "./Navigation";
 export { CartButton } from "./cart-button";
 export { MyAccountButton } from "./my-account-button";
